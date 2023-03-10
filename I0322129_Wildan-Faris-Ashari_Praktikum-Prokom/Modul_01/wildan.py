ad37f1862af90fca016af2c21e37eb8dccaccbc6
wildan <wildanfaris.ashari@student.uns.ac.id>
2023-03-10 14:46:09 +0700

    Commit_01

---
+++ I0322129_Wildan-Faris-Ashari_Praktikum-Prokom/Modul_01/wildan.py
@@ -0,0 +1,11 @@
+nama_depan = "wildan"
+nama_blk = "faris"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_D = ["wildan", "kls d", "dll"]
+dictionary = {"nama" : "Wildan", "kelas" : "Kls D"}
+
+print(nama_depan)
+print(f'nama saya adalah {nama_depan} {nama_blk}')
