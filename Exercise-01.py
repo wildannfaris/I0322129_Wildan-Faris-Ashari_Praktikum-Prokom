d5d47ff01de57eb15ce072dd2bcf5acb5e081681
wildan <wildanfaris.ashari@student.uns.ac.id>
2023-03-10 14:39:14 +0700

    Exsercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,14 @@
+nama = "Wildan Faris Ashari"
+nim = "I0322129"
+alamat = "Tanjungsari RT03/RW03, Ngesrep, Ngemplak, Boyolali"
+visualisasi = "Boyolali dikenal dengan julukan kota susu, dikarenakan susu sapi segar banyak diproduksi disini. Selain itu, sapi juga sebagai ikon Kota Boyolali"
+harapan = " Harapan saya sebagi lulusan Teknik Industri adalah bisa bekerja sesuai passion yang saya miliki"
+
+print("==============================================")
+print(f'Nama saya adalah : {nama}')
+print(f'NIM : {nim}')
+print(f'Alamat saya : {alamat}')
+print("==============================================")
+print(f'==> {visualisasi}')
+print(f'==> {harapan}')
+print("==============================================")
